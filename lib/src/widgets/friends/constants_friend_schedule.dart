d9a5f96f9042a9ddee30d42990ef4a6ea1144f33
Marcus Xavier <marcusxavierr123@gmail.com>
2020-08-08 22:49:46 -0300

    add constants

---
+++ lib/src/widgets/friends/constants_friend_schedule.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const TextStyle addFriendsTextTextStyle = TextStyle(
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w600,
+  color: Color(0xFF4D4848),
+);
+
+const TextStyle friendsWidgetScheduleTextStyle = TextStyle(
+  color: Color(0xFFFD7A7B),
+  fontSize: 17.0,
+  fontWeight: FontWeight.w600,
+  fontFamily: 'Roboto',
+);
